0246d35d1a5b29f632bd8e7b450dbb7c7de24937
Nashville Software School <nashvillesoftwareschool@Nashvilles-MacBook-Pro.local>
2017-04-20 15:53:59 -0500

    seond commit

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,4 +1,5 @@
-var famousPeople=[{title: "Actor", name: "Harrison Ford", bio: "American actor born in Chicago Illinois most noteable as playing Han Solo in the Star Wars series.",img: "https://en.wikipedia.org/wiki/Harrison_Ford#/media/File:Harrison_Ford_by_Gage_Skidmore_2.jpg", lifespan:{birth:1942,death: 2030}}, {
+var famousPeople=[{title: "Actor", name: "Harrison Ford", bio: "American actor born in Chicago Illinois most noteable as playing Han Solo in the Star Wars series.",image: "http://img2-1.timeinc.net/people/i/2015/news/150316/harrison-ford-7-435.jpg", lifespan:{birth:1942,death: "Present"}}, 
+{
   title: "Samurai",
   name: "Tomoe Gozen",
   bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
@@ -7,36 +8,51 @@ var famousPeople=[{title: "Actor", name: "Harrison Ford", bio: "American actor b
     birth: 1747,
     death: 1797
   }
-}, {
-  title: "Donald Trump",
-  name: "President",
+}, 
+{
+  title: "President",
+  name: "Donald Trump",
   bio: "Americas 45th elected president who took office in January 2017. Before he was better known as an entreprenuer and as a tv personality.",
-  image: "https://en.wikipedia.org/wiki/Donald_Trump#/media/File:Donald_Trump_official_portrait.jpg",
+  image: "http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg",
   lifespan: {
     birth: 1946,
-    death: 2035
+    death: "Present"
   }
 }
 
 ];
+var input= document.getElementById('input');
 var output= document.getElementById('output');
 var containerPerson= document.getElementsByClassName('person');
+var personName= document.getElementsByClassName('name');
+var person= document.getElementsByClassName('famousPerson')
 
 var counter=0;
 for (;counter<5;counter++){
 	output.innerHTML= `<div class="person" id="person--${counter}"></div>`;
 }
-for (var i=0; i<containerPerson.length; i++){
-	containerPerson[i].addEventListener('click', function(event){
 
+
+for (var i=0; i< famousPeople.length; i++){
+	containerPerson= `	<div class="famousPerson">
+	  	<h1 class="odd name">${famousPeople[i].name}</h1><h2 class="title even"> ${famousPeople[i].title}</h2>
+	  	<section class="odd bio">${famousPeople[i].bio}</section>
+	  	<img src="${famousPeople[i].image}" class="image even" alt="image of ${famousPeople[i].image}"> 
+	  	<footer id="lifespan" class="even">${famousPeople[i].lifespan.birth}-${famousPeople[i].lifespan.death}</footer>
+	</div>`;
+	output.innerHTML +=containerPerson;
+}
+for (var i=0; i<person.length; i++){
+	person[i].addEventListener('click', function(event){
+		event.target.classList.toggle('dotted');
+		input.focus();
 	});
 };
 
-for (var i=0; i< famousPeople.length; i++){
-	containerPerson= `	<person>
-	  	<header id="nameTitle" class="odd">${famousPeople[i].name} ${famousPeople[i].title}</header>
-	  	<section id="bioImage" class="even">${famousPeople[i].bio} ${famousPeople[i].img}</section>
-	  	<footer id="lifespan" class="odd">${famousPeople[i].lifespan.birth}-${famousPeople[i].lifespan.death}</footer>
-	</person>`;
-	output.innerHTML +=containerPerson;
-}
+
+
+
+
+
+
+
